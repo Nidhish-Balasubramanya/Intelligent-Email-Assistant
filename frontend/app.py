dc329fb3868c528886a410532e05b0e897eea8f2
Nidhish <nidhishbalasubramanya@gmail.com>
2025-11-22 13:41:51 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -21,8 +21,9 @@ Welcome to **OceanAI Email Agent**, your intelligent companion for email classif
 Use the sidebar to navigate through the application.
 
 Before using the application please warm up the backend by clicking on the below link and waiting for 2-3mins.
-st.markdown("[Click here](https://intelligent-email-assistant.onrender.com)")
+st.markdown("[warm up the backend](https://intelligent-email-assistant.onrender.com)")
 """)
+
 
 
 
